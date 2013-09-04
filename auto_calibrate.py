7554fe9dac6126f7cdf816d820628b7193dd5116
Malcolm Locke <malc@wholemeal.co.nz>
2013-09-04 20:43:20 +1200

    Add polynomial fit to autocrop peak detect

--- auto_calibrate.py
+++ auto_calibrate.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
 import pyfits
 import argparse
-import spectraplot
+import numpy as np
+import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description='Find the pixel location of the zero order center')
+parser = argparse.ArgumentParser(
+    description='Find the pixel location of the zero order center')
 parser.add_argument('filename', type=str, help='FITS filename')
-parser.add_argument('--outfile', type=str, help='Output filename', required=True)
+parser.add_argument('--outfile', type=str, help='Output filename',
+    required=True)
 parser.add_argument('--spacing', type=float, required=True,
     help='Channel spacing (Angstrom / pixel')
+parser.add_argument('--samplewidth', type=int, default=20,
+    help='Width each side of zero order peak to sample.')
+parser.add_argument('--degree', type=int, default=7,
+    help='Degree of polynomial fit')
+parser.add_argument('--visualise', action='store_true',
+    help='Visualise the zero order peak and fitted curve')
 
 args = parser.parse_args()
 
@@ -15,15 +24,25 @@ f = pyfits.open(args.filename)
 s = f[0].data.sum(axis=0)
 header = f[0].header
 
-# FIXME - Fit a curve here and find maxima
-maxpos = s.argmax()
+datamax = s.argmax()
+left = datamax - args.samplewidth
+right = datamax + args.samplewidth
+y = s[left:right]
+x = np.arange(len(y))
+z = np.polyfit(x, y, args.degree)
+p = np.poly1d(z)
+xp = np.linspace(0, len(y), 100)
+maxpos = xp[p(xp).argmax()] + left
+
+print '%s: Data peak: %d, polyfit peak: %f' % (args.filename, datamax, maxpos)
+
+if args.visualise:
+  plt.plot(x, y, '.', xp, p(xp), '-')
+  plt.show()
 
 header.update('CRVAL1', 0.0)
 header.update('CRPIX1', float(maxpos))
 header.update('CDELT1', args.spacing)
 header.update('CUNIT1', 'Angstrom')
 header.update('CTYPE1', 'Wavelength')
-
-print s
-
 pyfits.writeto(args.outfile, s.astype('int32'), header, output_verify='fix')
