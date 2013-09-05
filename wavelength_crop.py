1f12bf7257d2aa00c77e133388fa1bec5820ae25
Malcolm Locke <malc@wholemeal.co.nz>
2013-09-05 22:15:35 +1200

    Add script to wavelength crop spectra

---
+++ wavelength_crop.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+import pyfits
+import argparse
+import spectraplot
+import numpy as np
+
+parser = argparse.ArgumentParser(
+    description='Find the pixel location of the zero order center')
+parser.add_argument('filename', type=str, help='FITS filename')
+parser.add_argument('--outfile', type=str, help='Output filename',
+    required=True)
+parser.add_argument('--min', type=int, default=3800,
+    help='Minimum angstrom value')
+parser.add_argument('--max', type=int, default=8000,
+    help='Maximum angstrom value')
+
+args = parser.parse_args()
+
+f = pyfits.open(args.filename)
+header = f[0].header
+
+spectra = spectraplot.BessSpectra(f)
+calibration = spectra.calibration
+wl = np.array(spectra.wavelengths())
+left = np.argmax(wl>args.min)
+right = np.argmax(wl>args.max)
+
+print '%s: Cropping %d to %d' % (args.filename, left, right)
+
+cropped = spectra.data()[left:right]
+header.update('CRVAL1', calibration.angstrom(left+1))
+header.update('CRPIX1', 1.0)
+header.update('CRPLFT', left, 'Left of crop area from wavelength_crop.py')
+header.update('CRPRGT', right, 'Right of crop area from wavelength_crop.py')
+pyfits.writeto(args.outfile, cropped, header, output_verify='fix')
