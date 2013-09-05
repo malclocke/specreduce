de2af664e82bae66a78d940d1f70be355e19543a
Malcolm Locke <malc@wholemeal.co.nz>
2013-09-05 22:16:57 +1200

    Add script for stacking multiple spectra

---
+++ spectra_stack.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import pyfits
+import numpy as np
+import argparse
+import spectraplot
+
+parser = argparse.ArgumentParser(description='Stack spectra files')
+parser.add_argument('master', type=str)
+parser.add_argument('file', type=str, nargs='+')
+parser.add_argument('--outfile', '-o', type=str, required=True)
+
+args = parser.parse_args()
+
+data = []
+master = spectraplot.BessSpectra(pyfits.open(args.master))
+header = master.hdulist[0].header
+
+data.append(master.data())
+
+crvals = []
+
+for f in args.file:
+  s = spectraplot.BessSpectra(pyfits.open(f))
+  data.append(s.interpolate_to(master))
+
+dtype = master.hdulist[0].data.dtype.name
+mean_crval = np.mean(crvals)
+
+print 'Stacked %d frames' % (len(data))
+
+stacked = np.mean(data, axis=0)
+pyfits.writeto(args.outfile, stacked.astype(dtype), header)
