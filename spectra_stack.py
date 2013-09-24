ecf1267ffe8525898060dfb9928ffbc33850be63
Malcolm Locke <malc@wholemeal.co.nz>
2013-09-24 22:19:17 +1200

    Sum exposure time when stacking

--- spectra_stack.py
+++ spectra_stack.py
@@ -16,6 +16,11 @@ data = []
 master = specreduce.BessSpectra(pyfits.open(args.master))
 header = master.hdulist[0].header
 
+if 'EXPTIME' in header:
+  exptime = float(header['EXPTIME'])
+else:
+  exptime = 0.0
+
 data.append(master.data())
 
 crvals = []
@@ -23,6 +28,8 @@ crvals = []
 for f in args.file:
   s = specreduce.BessSpectra(pyfits.open(f))
   data.append(s.interpolate_to(master))
+  if 'EXPTIME' in s.header():
+    exptime += float(s.header()['EXPTIME'])
 
 dtype = master.hdulist[0].data.dtype.name
 mean_crval = np.mean(crvals)
@@ -31,4 +38,5 @@ print 'Stacked %d frames' % (len(data))
 
 stacked = np.mean(data, axis=0)
 header.update('NBADD', len(data), 'Number of coadded frames')
+header.update('EXPTIME', exptime)
 pyfits.writeto(args.outfile, stacked.astype(dtype), header)
--- stack.py
+++ stack.py
@@ -10,9 +10,23 @@ parser.add_argument('--outfile', '-o', type=str, required=True)
 
 args = parser.parse_args()
 
+exptime = 0.0
+header = False
+
 data = []
 for f in args.file:
-  data.append(pyfits.getdata(f))
+  hdulist = pyfits.open(f)
+  if header == False:
+    header = hdulist[0].header
+  if 'EXPTIME' in hdulist[0].header:
+    exptime += float(hdulist[0].header['EXPTIME'])
+  data.append(hdulist[0].data)
+
+if exptime > 0.0:
+  header.update('EXPTIME', exptime)
 
 master = np.mean(data, axis=0)
-pyfits.writeto(args.outfile, master.astype(np.uint8))
+
+print 'Stacked %d frames' % (len(data))
+
+pyfits.writeto(args.outfile, master.astype(np.uint8), header)
