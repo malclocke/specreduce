9a0d9d1791a38e1afe68f36d5127562a9993fd02
Malcolm Locke <malc@wholemeal.co.nz>
2013-12-15 22:17:41 +1300

    Add script to show decimal date of fits file

---
+++ decimal_date.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+import pyfits
+import argparse
+import dateutil.parser
+
+parser = argparse.ArgumentParser(
+    description='Show date with fractional day')
+parser.add_argument('filename', type=str, help='FITS filename')
+parser.add_argument('--keyword', '-k', type=str, default='DATE-OBS',
+    help='FITS header keyword for date.  Default: DATE-OBS')
+
+args = parser.parse_args()
+
+f = pyfits.open(args.filename)
+
+header = f[0].header
+timestamp = dateutil.parser.parse(header[args.keyword])
+
+month = timestamp.strftime('%b')
+
+day = timestamp.day
+
+seconds_expired_in_day = (timestamp.hour * 3600) + (timestamp.minute * 60) + timestamp.second
+fraction_of_day = float(seconds_expired_in_day) / 86400
+day_with_fraction = float(day) + fraction_of_day
+
+print '%d %s %.2f' % (timestamp.year, month, day_with_fraction)
